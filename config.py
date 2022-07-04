77284100f17e9cff00bd159ddea4d86c5bfb55c4
Tatiana <greenspider5918@mail.ru>
2022-07-05 03:39:56 +0800

    Add base connection

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 BOT_TOKEN = "5362218230:AAFyPYnpRNzZHZqnOvDlHBU7dL9FQxPUygU"
 APP_URL = "https://vsgutu04.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://zzmbwcutlwgwvv:05f5f73b960c2bdfe807568cd3b8d7eedacedc83da84c074fc4fc8079b33e11c@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d1sohlh3srhn03"
+
